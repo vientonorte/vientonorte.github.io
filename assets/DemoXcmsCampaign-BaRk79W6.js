211cc9cceae76b0ee237cbc47d83cf61edb1d431
vn-deploy <deploy@vientonorte.io>
2026-08-22 23:46:41 +0000

    deploy: Viento Norte SPA at domain root (from mi-portafolio, post-CI gate)

--- assets/DemoXcmsCampaign-BaRk79W6.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./vendor-motion-CDreM9qj.js";import t from"./TimedServiceDemo-BeG2l0Io.js";var n=e();function r(){return(0,n.jsx)(t,{forcedPath:`prototype`})}export{r as default};
